9566cb416e09610e9b07d42624c23dc2dd421c7d
Ogunlana Michael <56716012+MikeAdeO@users.noreply.github.com>
2021-10-01 09:02:50 +0100

    AUTH : Registration

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
-// dotenv.config();
-// mongoose.connect(process.env.MONGO_URL).then(console.log('connected to monogdb')).catch((err) => console.log(err));
+dotenv.config();
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URL).then(console.log('connected to monogdb')).catch((err) => console.log(err));
+ 
 
-app.use('/', (req, res)=>{
-     console.log('this is the main url');
-});
+app.use('/api/auth', authRoute);
 app.listen('5000',() =>{
      console.log('this app runs on port 5000');
 });
